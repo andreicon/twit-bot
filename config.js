604b3f5b05bb309a189f4ab22d3d702080c867bd
Hydra Crunch <andrei.costea47@gmail.com>
2016-11-28 07:58:43 +0000

    save initial effort

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: 'fyo1UbSq1VqqSaHJ6WIGIkv65',  
+  consumer_secret: 'RlkdoN6dYUAJSE6fuv130z1o23gfVC722HWJaTxvxjXZ2bWzxo',
+  access_token: '749151876-NgnDCa5ZyiGjIpeyW5tL4XCHz1Z4y54Om0CuvcAc',  
+  access_token_secret: 'IGFxmTi1rKzVxezWrFyeVcfFb8nczKCYyxjaqWeoqpfkE'
+}
